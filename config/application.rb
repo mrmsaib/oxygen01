80cd2002ad0db4326f98b5c29f3b5e214fdc83ec
MO. H <msaib_prog@yahoo.com>
2014-12-03 01:23:46 -0700

    utf8_unicode_ci to Application.rb

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,5 @@ module Oxygenarabi
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    ActiveRecord::Base.connection.collation "SET collation_database ='utf8_general_ci' "
-    ActiveRecord::Base.connection.collation "SET collation_connection ='utf8_general_ci' "
   end
 end
